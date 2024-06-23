217d7122642fbdcb36218e75593b93dbc29be88b
CodePapayas <catspianos@gmail.com>
2024-06-22 22:28:25 -0700

    Turn card game into class

--- GoFish.py
+++ GoFish.py
@@ -2,55 +2,62 @@ from card import *
 from termcolor import colored, cprint
 from prettytable import PrettyTable
 
-deck = build_deck()
-player_deck = []
-player_match_pile = []
-player_table = PrettyTable()
-ai_deck = []
-ai_match_pile = []
-ai_table = PrettyTable()
+class CardGame:
+    def __init__(self):
+        self.deck = build_deck()
+        self.player_deck = []
+        self.player_match_pile = []
+        self.player_table = PrettyTable()
+        self.ai_deck = []
+        self.ai_match_pile = []
+        self.ai_table = PrettyTable()
 
-# Set up table headers
-player_table.field_names = ["Player Hand"]
-ai_table.field_names = ["AI Hand"]
+        # Set up table headers
+        self.player_table.field_names = ["Player Hand"]
+        self.ai_table.field_names = ["AI Hand"]
 
-# Deal cards to player and AI
-dealer_tracker = 1
-cards_dealt = 0
+    def deal_cards(self):
+        dealer_tracker = 0
+        cards_dealt = 0
 
-while cards_dealt < 14:
-    dealt_card = deck.pop(0)
-    if dealer_tracker % 2 == 0:
-        player_deck.append(dealt_card)
-    else:
-        ai_deck.append(dealt_card)
-    dealer_tracker += 1
-    cards_dealt += 1
+        while cards_dealt < 14:
+            dealt_card = self.deck.pop(0)
+            if dealer_tracker % 2 == 0:
+                self.player_deck.append(dealt_card)
+            else:
+                self.ai_deck.append(dealt_card)
+            dealer_tracker += 1
+            cards_dealt += 1
 
-# Add player cards to the table with colors
-for card in player_deck:
-    if card.get_suit() == 'Red':
-        player_table.add_row([colored(card.get_value(), "red")])
-    elif card.get_suit() == 'Blue':
-        player_table.add_row([colored(card.get_value(), "blue")])
-    elif card.get_suit() == 'Yellow':
-        player_table.add_row([colored(card.get_value(), "yellow")])
-    elif card.get_suit() == 'Green':
-        player_table.add_row([colored(card.get_value(), "green")])
+    def populate_tables(self):
+        for card in self.player_deck:
+            if card.get_suit() == 'Red':
+                self.player_table.add_row([colored(card.get_value(), "red")])
+            elif card.get_suit() == 'Blue':
+                self.player_table.add_row([colored(card.get_value(), "blue")])
+            elif card.get_suit() == 'Yellow':
+                self.player_table.add_row([colored(card.get_value(), "yellow")])
+            elif card.get_suit() == 'Green':
+                self.player_table.add_row([colored(card.get_value(), "green")])
 
-# Add AI cards to the table with colors
-for card in ai_deck:
-    if card.get_suit() == 'Red':
-        ai_table.add_row([colored(card.get_value(), "red")])
-    elif card.get_suit() == 'Blue':
-        ai_table.add_row([colored(card.get_value(), "blue")])
-    elif card.get_suit() == 'Yellow':
-        ai_table.add_row([colored(card.get_value(), "yellow")])
-    elif card.get_suit() == 'Green':
-        ai_table.add_row([colored(card.get_value(), "green")])
+        for card in self.ai_deck:
+            if card.get_suit() == 'Red':
+                self.ai_table.add_row([colored(card.get_value(), "red")])
+            elif card.get_suit() == 'Blue':
+                self.ai_table.add_row([colored(card.get_value(), "blue")])
+            elif card.get_suit() == 'Yellow':
+                self.ai_table.add_row([colored(card.get_value(), "yellow")])
+            elif card.get_suit() == 'Green':
+                self.ai_table.add_row([colored(card.get_value(), "green")])
 
-# Print the tables
-print("AI Hand:")
-print(ai_table)
-print("\nPlayer Hand:")
-print(player_table)
+    def print_tables(self):
+        print("AI Hand:")
+        print(self.ai_table)
+        print("\nPlayer Hand:")
+        print(self.player_table)
+
+# Instantiate the game and run it
+game = CardGame()
+game.deal_cards()
+game.populate_tables()
+game.print_tables()
